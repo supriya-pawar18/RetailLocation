9fb86028aa3a1bb636f7359529591a80c555abaf
VegatechSupriya <99873631+VegatechSupriya@users.noreply.github.com>
2023-07-23 16:46:18 +0530

    this is first project of retail

---
+++ src/test/java/com/BikkadIt/RetailLocation/RetailLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.RetailLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
